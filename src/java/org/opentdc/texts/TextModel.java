aa5633ef8e5deab638d5f188148dbdf9478b0971
Bruno Kaiser <bruno@bkaiser.ch>
2015-07-22 15:00:50 +0200

    initial state, created by ant createService

---
+++ src/java/org/opentdc/texts/TextModel.java
@@ -0,0 +1,205 @@
+/**
+ * The MIT License (MIT)
+ *
+ * Copyright (c) 2015 Arbalo AG
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+package org.opentdc.texts;
+
+import java.util.Comparator;
+import java.util.Date;
+
+import javax.xml.bind.annotation.XmlAccessType;
+import javax.xml.bind.annotation.XmlAccessorType;
+import javax.xml.bind.annotation.XmlRootElement;
+
+/**
+ * @author Bruno Kaiser
+ *
+ */
+@XmlAccessorType(XmlAccessType.FIELD)
+@XmlRootElement
+public class TextModel {
+
+	private String id;		// sortable
+	private String title;	// mandatory
+	private String description;
+	private Date createdAt;
+	private String createdBy;
+	private Date modifiedAt;
+	private String modifiedBy;
+
+	/**
+	 * Empty constructor.
+	 */
+	public TextModel() {
+	}
+
+	/**
+	 * Constructor.
+	 * @param title	the title (mandatory)
+	 * @param description	a description
+	 */
+	public TextModel(String title, String description) {
+		this.title = title;
+		this.description = description;
+	}
+
+	/**
+	 * Get the id.
+	 * @return the id.
+	 */
+	public String getId() {
+		return id;
+	}
+
+	/**
+	 * Set the id.
+	 * @param id the id to set, normally this is done on the server only (unique id).
+	 */
+	public void setId(String id) {
+		this.id = id;
+	}
+
+	/**
+	 * Get the title.
+	 * @return the title
+	 */
+	public String getTitle() {
+		return title;
+	}
+
+	/**
+	 * Set the title.
+	 * @param title  the title to set
+	 */
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+	/**
+	 * Get the description.
+	 * @return the description
+	 */
+	public String getDescription() {
+		return description;
+	}
+
+	/**
+	 * Set the description.
+	 * @param description  the description to set
+	 */
+	public void setDescription(String description) {
+		this.description = description;
+	}
+/**
+	 * Get the creation date.
+	 * @return the creation date
+	 */
+	public Date getCreatedAt() {
+		return createdAt;
+	}
+
+	/**
+	 * Set the creation date. This attibute should only be set once at creation time and exclusively by the server.
+	 * @param createdAt the date when this object was created.
+	 */
+	public void setCreatedAt(Date createdAt) {
+		this.createdAt = createdAt;
+	}
+
+	/** 
+	 * Get the creation date.
+	 * @return the creation date
+	 */
+	public String getCreatedBy() {
+		return createdBy;
+	}
+
+	/**
+	 * Set the createdBy attribute, i.e. the user that created the attribute.
+	 * This attribute should only be set once at creation time and exclusively by the server.
+	 * @param createdBy  the loginId of the user having created the object
+	 */
+	public void setCreatedBy(String createdBy) {
+		this.createdBy = createdBy;
+	}
+
+	/**
+	 * Get the date of the last modification. 
+	 * @return the date of the last modification
+	 */
+	public Date getModifiedAt() {
+		return modifiedAt;
+	}
+
+	/**
+	 * Set the modification date. This should only be change by the server when updating an object.
+	 * @param modifiedAt  the date of the last modification
+	 */
+	public void setModifiedAt(Date modifiedAt) {
+		this.modifiedAt = modifiedAt;
+	}
+
+	/**
+	 * Get the modifiedBy attribute, i.e. the loginId of the user who made the last update.
+	 * @return modifiedBy the loginId of the user who made the last update
+	 */
+	public String getModifiedBy() {
+		return modifiedBy;
+	}
+
+	/**
+	 * Set the modifiedBy attribute, i.e. the loginId of the user who made the last update.
+	 * @param modifiedBy the loginId of the user who made the last update
+	 */
+	public void setModifiedBy(String modifiedBy) {
+		this.modifiedBy = modifiedBy;
+	}
+	
+	/******************************* Comparator *****************************/
+	/**
+	 * Comparator for comparing two rates based on their id.
+	 */
+	public static Comparator<TextModel> TextComparator = new Comparator<TextModel>() {
+
+		/* (non-Javadoc)
+		 * @see java.util.Comparator#compare(java.lang.Object, java.lang.Object)
+		 * @return -1 if the first object is null of smaller; otherwise it returns +1
+		 */
+		public int compare(TextModel obj1, TextModel obj2) {
+			if (obj1.getId() == null) {
+				return -1;
+			}
+			if (obj2.getId() == null) {
+				return 1;
+			}
+
+			String _attr1 = obj1.getId();
+			String _attr2 = obj2.getId();
+
+			// ascending order
+			return _attr1.compareTo(_attr2);
+
+			// descending order
+			// return _attr2.compareTo(_attr1);
+		}
+	};
+}
---
+++ src/java/org/opentdc/texts/TextsService.java
@@ -0,0 +1,138 @@
+/**
+ * The MIT License (MIT)
+ *
+ * Copyright (c) 2015 Arbalo AG
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+package org.opentdc.texts;
+
+import java.util.List;
+import java.util.logging.Logger;
+
+// import io.swagger.annotations.*;
+
+import javax.servlet.ServletContext;
+import javax.ws.rs.Consumes;
+import javax.ws.rs.DELETE;
+import javax.ws.rs.DefaultValue;
+import javax.ws.rs.GET;
+import javax.ws.rs.POST;
+import javax.ws.rs.PUT;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
+import javax.ws.rs.QueryParam;
+import javax.ws.rs.core.Context;
+import javax.ws.rs.core.MediaType;
+
+import org.opentdc.service.GenericService;
+import org.opentdc.service.exception.DuplicateException;
+import org.opentdc.service.exception.InternalServerErrorException;
+import org.opentdc.service.exception.NotFoundException;
+import org.opentdc.service.exception.ValidationException;
+
+/*
+ * @author Bruno Kaiser
+ */
+@Path("/api/text")
+// @Api(value = "/api/text", description = "Operations about texts")
+@Consumes(MediaType.APPLICATION_JSON)
+@Produces(MediaType.APPLICATION_JSON)
+public class TextsService extends GenericService<ServiceProvider> {
+	
+	private static ServiceProvider sp = null;
+
+	private static final Logger logger = Logger.getLogger(TextsService.class.getName());
+
+	/**
+	 * Invoked for each service invocation (Constructor)
+	 * @throws ReflectiveOperationException
+	 */
+	public TextsService(
+		@Context ServletContext context
+	) throws ReflectiveOperationException{
+		logger.info("> TextsService()");
+		if (sp == null) {
+			sp = this.getServiceProvider(TextsService.class, context);
+		}
+		logger.info("TextsService() initialized");
+	}
+
+	/******************************** texts *****************************************/
+	@GET
+	@Path("/")
+//	@ApiOperation(value = "Return a list of all texts", response = List<TextModel>.class)
+	public List<TextModel> list(
+		@DefaultValue(DEFAULT_QUERY_TYPE) @QueryParam("queryType") String queryType,
+		@DefaultValue(DEFAULT_QUERY) @QueryParam("query") String query,
+		@DefaultValue(DEFAULT_POSITION) @QueryParam("position") int position,
+		@DefaultValue(DEFAULT_SIZE) @QueryParam("size") int size
+	) {
+		return sp.list(queryType, query, position, size);
+	}
+
+	@POST
+	@Path("/")
+	//	@ApiOperation(value = "Create a new text", response = TextModel.class)
+	//	@ApiResponses(value = 
+	//			{ @ApiResponse(code = 409, message = "An object with the same id exists already (CONFLICT)") },
+	//			{ @ApiResponse(code = 400, message = "Invalid ID supplied or mandatory field missing (BAD_REQUEST)" })
+	public TextModel create(
+		TextModel text) 
+	throws DuplicateException, ValidationException {
+		return sp.create(text);
+	}
+
+	@GET
+	@Path("/{id}")
+	//	@ApiOperation(value = "Find a text by id", response = TextModel.class)
+	//	@ApiResponses(value = { 
+	//			@ApiResponse(code = 405, message = "An object with the given id was not found (NOT_FOUND)" })
+	public TextModel read(
+		@PathParam("id") String id
+	) throws NotFoundException {
+		return sp.read(id);
+	}
+
+	@PUT
+	@Path("/{id}")
+	//	@ApiOperation(value = "Update the text with id with new values", response = TextModel.class)
+	//	@ApiResponses(value =  
+	//			{ @ApiResponse(code = 405, message = "An object with the given id was not found (NOT_FOUND)" },
+	//			{ @ApiResponse(code = 400, message = "Invalid new values given or trying to change immutable fields (BAD_REQUEST)" })
+	public TextModel update(
+		@PathParam("id") String id,
+		TextModel text
+	) throws NotFoundException, ValidationException {
+		return sp.update(id, text);
+	}
+
+	@DELETE
+	@Path("/{id}")
+	//	@ApiOperation(value = "Delete the text with the given id" )
+	//	@ApiResponses(value =  
+	//			{ @ApiResponse(code = 405, message = "An object with the given id was not found (NOT_FOUND)" },
+	//			{ @ApiResponse(code = 500, message = "Data inconsistency found (INTERNAL_SERVER_ERROR)" })
+	public void delete(
+		@PathParam("id") String id) 
+	throws NotFoundException, InternalServerErrorException {
+		sp.delete(id);
+	}
+}
